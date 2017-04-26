6f766a7971e35c6c0f4efd223731a1f61d78a291
volchanka <lyudmila@iMac-IamiMacAdmin.local>
2017-04-26 18:45:11 +0300

    5-6.1

--- 05 Case - DNA.playground/Contents.swift
+++ 05 Case - DNA.playground/Contents.swift
@@ -7,39 +7,81 @@ import Foundation
 //
 
 // 1. Сделайте описание элемента (нуклеотид) ДНК с 4 возможными типами нуклеотидов, создайте пару примеров
-enum Nuk {
-    case A, G, T, C
+enum Nuk : Int{
+    case Adenin, Guanin, Timin, Citozin
+    static var count: Int { return Nuk.Citozin.hashValue + 1} //cool hack
 }
 
-let nuk1 = Nuk.A
-let nuk2 = Nuk.C
+let nuk1 = Nuk.Adenin
+let nuk2 = Nuk.Citozin
 
 // 2. Сделайте экстеншн для нуклеотида, который поможет вам потом с функцией print(...), выведите результат в консоль из п. 1
 
 extension Nuk {
-    func name() -> String{
-        switch self {
-        case .A:
-            return "Adenin"
-        case .G:
-            return "Guanin"
-        case .T:
-            return "Timin"
-        case .C:
-            return "Citozin"
+    var string: String { return String(describing: self) }
+}
+
+
+print(nuk1.string)
+print(nuk2.string)
+
+// 3. Сделайте описание ДНК, который содержит набор нуклеотидов, создайте пару примеров
+
+
+struct DNA {
+    var list: [Nuk] = []
+    init(nuks: [Nuk]) {
+        list = nuks
+    }
+}
+
+let dna1 = DNA.init(nuks: [nuk1, nuk2])
+let dna2 = DNA.init(nuks: [Nuk.Guanin, Nuk.Adenin, Nuk.Adenin])
+
+
+//// 4. Сделайте экстеншн для ДНК, который поможет вам потом с функцией print(...), выведите результат в консоль из п. 3
+
+extension DNA {
+    func name() -> String {
+        var allNuklsNames : String = ""
+        for item in self.list {
+            allNuklsNames.append(item.string + " ")
+        }
+        return allNuklsNames
+    }
+}
+print(dna1.name())
+print(dna2.name())
+
+//// 5. Сделайте так, чтобы можно было создавать случайную цепочку ДНК определенной длины
+
+extension DNA{
+    init(lenth:Int) {
+        for _ in 1...lenth {
+            let randomIndex = Int(arc4random_uniform(UInt32(Nuk.count)))
+            list.append(Nuk(rawValue:randomIndex)!)
         }
     }
 }
 
-print(nuk1.name())
-print(nuk2.name())
+let randomDNA = DNA(lenth: 16)
+print(randomDNA.name())
 
-// 3. Сделайте описание ДНК, который содержит набор нуклеотидов, создайте пару примеров
-// 4. Сделайте экстеншн для ДНК, который поможет вам потом с функцией print(...), выведите результат в консоль из п. 3
-// 5. Сделайте так, чтобы можно было создавать случайную цепочку ДНК определенной длины
+
 // 6. Мутация (рождение ребенка)
+
 // 6.1 Создайте 2 случайных ДНК, каждая из которых содержит 30 нуклеотидов (уловно - мама и папа), выведите их в консоль
+
+let mama = DNA(lenth: 30)
+let papa = DNA(lenth: 30)
+
+print("Mama = " + mama.name())
+print("Papa = " + papa.name())
+
 // 6.2 Правила мутации - сравниваем элементы в обоих днк попарно в массивах из п.6.1
+
+
+
 // 6.3 С вероятностью 40% результат будет иметь ген или "папы" или "мамы" (итоговая вероятность - 80%)
 // 6.4 Иначе, результат приобретает случайный ген (вероятность 20%)
 // 6.5 Создайте функцию, принимающую две ДНК на входе и отдающую результирующую ДНК (после мутации) на выходе.
